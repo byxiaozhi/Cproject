d2efdfcd1114b3c937e8f331c09ab30571f854e1
ZZF <31278216+byxiaozhi@users.noreply.github.com>
2020-09-27 23:37:55 +0800

    修复错误

--- Record.c
+++ Record.c
@@ -7,5 +7,5 @@ void recordManage()
         system("cls");
         SetConsoleTitle("诊疗信息管理");
     }
-    while(selector(5,"新建挂号","检查","开药","住院","记录查询","返回",NULL,NULL,NULL,NULL,NULL)!=6);
+    while(selector(6,"新建挂号","检查","开药","住院","记录查询","返回",NULL,NULL,NULL,NULL,NULL)!=6);
 }
